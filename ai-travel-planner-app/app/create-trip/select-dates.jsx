b7b92646cb350571cc238f357be1649eb286078c
Achintha0626 <124795790+Achintha0626@users.noreply.github.com>
2025-02-18 23:06:48 +0530

    complete Select Traveler Date Feature

--- ai-travel-planner-app/app/create-trip/select-dates.jsx
+++ ai-travel-planner-app/app/create-trip/select-dates.jsx
@@ -1,20 +1,59 @@
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ToastAndroid,
+} from "react-native";
+import React, { useContext, useState } from "react";
 import { useNavigation, useRouter } from "expo-router";
 import { Colors } from "../../constants/Colors";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import CalendarPicker from "react-native-calendar-picker";
+import moment from "moment";
+import { Alert, Platform } from "react-native";
+import { CreateTripContext } from "../../context/CreateTripContext";
 
 export default function SelectDates() {
   const navigation = useNavigation();
   const router = useRouter();
 
   // Add state to store the selected date
-  const [selectedDate, setSelectedDate] = useState(null);
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
+  const { tripData, setTripData } = useContext(CreateTripContext);
 
   // Define the onDateChange function
-  const onDateChange = (date) => {
-    setSelectedDate(date);
+  const onDateChange = (date, type) => {
+    console.log(date, type);
+    if (type == "START_DATE") {
+      setStartDate(moment(date));
+    } else {
+      setEndDate(moment(date));
+    }
+  };
+
+  const OnDateSelectionContinue = () => {
+    if (!startDate || !endDate) {
+      const message = "Please select Start date and End date";
+
+      if (Platform.OS === "android") {
+        ToastAndroid.show(message, ToastAndroid.LONG);
+      } else {
+        Alert.alert("Alert", message);
+      }
+
+      return;
+    }
+
+    const totalNoOfDays = endDate.diff(startDate, "days");
+    console.log(totalNoOfDays + 1);
+    setTripData({
+      ...tripData,
+      startDate: startDate,
+      endDate: endDate,
+      totalNoOfDays: totalNoOfDays + 1,
+    });
   };
 
   return (
@@ -36,12 +75,20 @@ export default function SelectDates() {
             color: Colors.WHITE,
           }}
         />{" "}
+        <View>
+          <TouchableOpacity
+            style={styles.opacity}
+            onPress={OnDateSelectionContinue}
+          >
+            <Text style={styles.button}>Continue</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-      {selectedDate && (
+      {/* {selectedDate && (
         <Text style={styles.selectedDate}>
           Selected Date: {selectedDate.toString()}
         </Text>
-      )}
+      )} */}
     </View>
   );
 }
@@ -66,5 +113,17 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontFamily: "outfit-medium",
     color: Colors.PRIMARY,
+  },
+  opacity: {
+    padding: 15,
+    backgroundColor: Colors.PRIMARY,
+    borderRadius: 15,
+    marginTop: 35,
+  },
+  button: {
+    textAlign: "center",
+    color: Colors.WHITE,
+    fontFamily: "outfit-medium",
+    fontSize: 20,
   },
 });
